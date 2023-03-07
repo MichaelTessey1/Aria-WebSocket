c6648b1e8963507e5fd6dbbb6a53d2db65fe0599
Michaeltessey1 <michaeltessey1@gmail.com>
2023-03-06 21:53:58 -0500

    websockets working

--- server.js
+++ server.js
@@ -7,6 +7,19 @@ const io = require('socket.io')(8000, {
 io.on('connection', socket => {
     console.log(socket.id)
     socket.on("room-key", (roomKey) => {
-        console.log(roomKey)
+        console.log("rk:", roomKey, "sid:", socket.id)
+        //send to everyone connected to the server including yourself
+        // io.emit('receive-roomKey', roomKey)
+        //for everyone else
+        // socket.broadcast.emit('receive-roomKey', roomKey)
+        //to a specific room
+        if(roomKey === "") {
+            socket.broadcast.emit('receive-roomKey', roomKey)
+            console.log('random non existent room key')
+        }
+        else {
+            socket.to(roomKey).emit('receive-roomKey', roomKey)
+            console.log('WORKING ROOM SOCKET')
+        }
     }) 
 })
